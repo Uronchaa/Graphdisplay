83822ec6530f8e8fab7463f3b01ac2182ede04a6
ade <ade@ade.com>
2021-05-12 20:26:02 +0200

    graphlist double click

--- ui/graphlist_widget.py
+++ ui/graphlist_widget.py
@@ -1,14 +1,26 @@
 from PyQt5.QtWidgets import QListWidget, QListWidgetItem
+from PyQt5 import QtGui
+from PyQt5 import QtCore
 
 
 class GraphlistView(QListWidget):
     def __init__(self, parent=None):
         super(GraphlistView, self).__init__(parent)
 
+        self._red = QtGui.QBrush(QtCore.Qt.red)
+        self._green = QtGui.QBrush(QtCore.Qt.green)
+
+        self.itemDoubleClicked.connect(self._handleDoubleClick)
+
     def setdata(self, list):
         for item in list:
             i = QListWidgetItem(str(item))
             self.addItem(i)
+
+    def _handleDoubleClick(self, item: QListWidgetItem):
+        color = item.background()
+        item.setBackground(self._red if color == self._green else self._green)
+        item.setSelected(False)
 
 
 class GraphlistCtrl:
@@ -16,9 +28,14 @@ class GraphlistCtrl:
         self.model = model
         self.view = GraphlistView()
 
+        self.view.itemDoubleClicked.connect(self._handledoubleclick)
+
     def update_data(self, list):
         self.view.clear()
         self.view.setdata(list)
+
+    def _handledoubleclick(self, item):
+        print(self.view.currentRow())
 
 
 if __name__ == "__main__":
