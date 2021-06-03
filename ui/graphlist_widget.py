4c3cede48856c017c88c2e677e2eba222e8f9350
ade <ade@ade.com>
2021-06-03 12:52:49 +0200

    standalone graphlist widget

--- ui/graphlist_widget.py
+++ ui/graphlist_widget.py
@@ -1,9 +1,12 @@
 from PyQt5.QtWidgets import QListWidget, QListWidgetItem
 from PyQt5 import QtGui
 from PyQt5 import QtCore
+from warnings import warn
 
 
 class GraphlistView(QListWidget):
+    updated = QtCore.pyqtSignal()
+
     def __init__(self, parent=None):
         super(GraphlistView, self).__init__(parent)
 
@@ -13,29 +16,47 @@ class GraphlistView(QListWidget):
         self.itemDoubleClicked.connect(self._handleDoubleClick)
 
     def setdata(self, list):
+        self.clear()
         for item in list:
             i = QListWidgetItem(str(item))
             self.addItem(i)
+
+    def get_list_status(self):
+        out = []
+        for i in self.iterAllItems():
+            meta = {"name": i.text()}
+            if i.background() == self._green:
+                meta["visible"] = True
+            else:
+                meta["visible"] = True
+            out.append(meta)
+        return out
 
     def _handleDoubleClick(self, item: QListWidgetItem):
         color = item.background()
         item.setBackground(self._red if color == self._green else self._green)
         item.setSelected(False)
+        self.updated.emit()
+
+    def iterAllItems(self):
+        for i in range(self.count()):
+            yield self.item(i)
 
 
 class GraphlistCtrl:
-    def __init__(self, model):
-        self.model = model
+    warn("GraphlistCtrl is deprecated", DeprecationWarning)
+
+    def __init__(self):
         self.view = GraphlistView()
 
-        self.view.itemDoubleClicked.connect(self._handledoubleclick)
+        self.view.updated.connect(self._handleviewupdated)
 
     def update_data(self, list):
         self.view.clear()
         self.view.setdata(list)
 
-    def _handledoubleclick(self, item):
-        print(self.view.currentRow())
+    def _handleviewupdated(self):
+        print(self.view.get_list_status())
 
 
 if __name__ == "__main__":
@@ -45,7 +66,7 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     names = ["poi", "oiupo", "opyiopyh"]
-    m = GraphlistCtrl(None)
-    m.update_data(names)
-    m.view.show()
+    m = GraphlistView()
+    m.setdata(names)
+    m.show()
     sys.exit(app.exec_())
